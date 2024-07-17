4ae960783eeaf830b2e6a88379013d40acc989e3
Guilherme Almeida <guidevtec@gmail.com>
2024-07-17 18:21:01 -0300

    "subindo alterações"

---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPOO.Models;
+//erro pois a classe é abstrata
+//Smartphone smartphone= new Smartphone();
+Iphone iphone=new Iphone("2212","iPhone 14 Pro","33333",16);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagran");
+Nokia nokia=new Nokia("7080","Nokia 8.3 5G","7777",5);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Jogo da cobrinha");
